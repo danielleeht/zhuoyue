a6a94ad5afd1d6dd052e26ce96ae0cccb66d7cd1
danielleeht <danielleeht@gmail.com>
2016-10-18 20:42:40 +0800

    api测试

--- zhuoyue-book/src/main/java/com/zhuoyue/book/domain/author/BookAuthor.java
+++ zhuoyue-book/src/main/java/com/zhuoyue/book/domain/author/BookAuthor.java
@@ -1,6 +1,8 @@
 package com.zhuoyue.book.domain.author;
 
 import com.zhuoyue.commons.BaseEntity;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
 
@@ -11,9 +13,36 @@ import javax.persistence.*;
 @Entity
 public class BookAuthor extends BaseEntity {
 
+    private String name;    //姓名
+
     @ManyToOne(fetch = FetchType.EAGER)
+    @NotFound(action= NotFoundAction.IGNORE)
     private Author author;
 
     @Enumerated(EnumType.STRING)
     private BookAuthorType bookAuthorType;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public BookAuthorType getBookAuthorType() {
+        return bookAuthorType;
+    }
+
+    public void setBookAuthorType(BookAuthorType bookAuthorType) {
+        this.bookAuthorType = bookAuthorType;
+    }
 }
